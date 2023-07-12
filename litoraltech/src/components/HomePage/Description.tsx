c865c177a942a49ca47fd0d0f4d736a430181350
Marcelorox <marcelo.paladino01@gmail.com>
2023-07-12 11:01:07 -0400

    new: button hover

--- litoraltech/src/components/HomePage/Description.tsx
+++ litoraltech/src/components/HomePage/Description.tsx
@@ -7,11 +7,11 @@ const Description: React.FC<Props> = () => {
   return (
     <div className="flex flex-col justify-center h-[80vh] md:h-[100vh] md:flex-row">
       <div className="flex flex-col md:mt-[10rem] md:justify-normal">
-        <div className="flex justify-center flex-col items-center">
+        <div className="flex flex-col items-center justify-center">
           <Image src="/logoTech.svg" alt="Logo" width={120} height={120} />
-          <h2 className="text-white font-bold text-2xl md:text-5xl mt-3 text-center">Inovation, Security and Quality!</h2>
+          <h2 className="mt-3 text-2xl font-bold text-center text-white md:text-5xl">Inovation, Security and Quality!</h2>
         </div>
-    <div className="flex w-full justify-center">
+    <div className="flex justify-center w-full">
         <span className="text-white md:w-[50vw] items-center text-center mt-4">
           We place the client at the center of our operations. We understand
           that each organization has unique needs, and as such, we develop
@@ -22,7 +22,7 @@ const Description: React.FC<Props> = () => {
 
 
         <div className="flex justify-center mt-5">
-          <button className="text-white w-80 p-1 bg-[#0085FF] rounded">FREE CONSULT</button>
+          <button className="text-white w-80 p-1 bg-[#0085FF] rounded  hover:bg-blue-500 transition hover:scale-110">FREE CONSULT</button>
         </div>
 
       </div>
