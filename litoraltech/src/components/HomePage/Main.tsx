00230d13a2fac05572dec0d07a5032b4fc1164d5
Marcelorox <marcelo.paladino01@gmail.com>
2023-07-12 13:28:38 -0400

    fix: responsive screen

--- litoraltech/src/components/HomePage/Main.tsx
+++ litoraltech/src/components/HomePage/Main.tsx
@@ -11,7 +11,7 @@ const Main = () => {
     <main className="flex flex-col bg-[url('/paisagem.gif')] bg-contain">
         <Header/>
         <Description/>
-        <div className="absolute h-[15rem] top-[80vh] md:top-[83vh] w-full md:h-60 bg-[url('/Vector.svg')] bg-cover"></div>
+        <div className="absolute h-[15rem] top-[80vh] md:top-[83vh] w-full md:h-60 bg-[url('/Vector.svg')] bg-cover lg:top-[85vh]"></div>
         <AboutUs/>
     </main>
     
